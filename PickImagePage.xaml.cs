2bf4c392fab3af2f80be9d22fa183707c5bf7f81
Ibrahim AlDekhail <33824044+idekhail@users.noreply.github.com>
2021-11-07 11:10:47 +0300

    Add files via upload

---
+++ PickImagePage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Essentials;
+
+
+namespace XF_MediaPicker
+{
+    public partial class PickImagePage : ContentPage
+        {
+            public PickImagePage()
+            {
+                InitializeComponent();
+            }
+
+            async void PickImg_Clicked(System.Object sender, System.EventArgs e)
+            {
+                var result = await MediaPicker.PickPhotoAsync(new MediaPickerOptions
+                {
+                    Title = "Please pick a photo"
+                });
+
+                if (result != null)
+                {
+                    var stream = await result.OpenReadAsync();
+
+                    resultImage.Source = ImageSource.FromStream(() => stream);
+                }
+            }
+
+            async void TakeImg_Clicked(System.Object sender, System.EventArgs e)
+            {
+                var result = await MediaPicker.CapturePhotoAsync();
+
+                if (result != null)
+                {
+                    var stream = await result.OpenReadAsync();
+
+                    resultImage.Source = ImageSource.FromStream(() => stream);
+                }
+            }
+        }
+    }
